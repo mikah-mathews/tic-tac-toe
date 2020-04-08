7880686aabdc3e7ebbf2f5512537ca2783ecbf19
Matt Stroud <lixingke06@gmail.com>
2020-04-08 13:34:04 -0700

    work on dynamically creating board grid

--- js/scripts.js
+++ js/scripts.js
@@ -19,32 +19,40 @@
 //3b. if boxID[3]=o subtract 1 from winArray[4]
 //4. 
 
-// row, col = 1, 1
-// while row < 4:
-//     col = 1
-//     while col < 4:
-//         if row != col:
-//             print(row, col)
-//         col += 1
-//     row += 1     
+   
 
-var row = 1
-var col = 1
-var box = []
-var grid = []
-for (row=1; row<4; row++) {
-  col = 1;    
-  for (col=1; col<4; col++) {
-    box = [row, col];
-    grid.push(box); 
+makeGrid = function() {
+  var row = 1
+  var col = 1
+  var box = []
+  var grid = []
+  for (row=1; row<4; row++) {
+    col = 1;    
+    for (col=1; col<4; col++) {
+      box = [row, col];
+      grid.push(box); 
+    }
+    return grid
   }
-  console.log(grid)
 }
-
 
 function Board() {
-  boxIDs=[[1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3]];
+
+
+function Box(row, col) {  // constructor
+  this.row = row;
+  this.col = col;
 }
+  
+// USE THIS IDEA LATER FOR CACLULATING WINS
+// squareVal = function(grid) {
+//   var boxVal=""
+//    for (i=0; i<grid.length; i++) {
+//     box.toString()=boxVal
+//      
+//   }
+// +}
+// }
 var winArrays = [[[1,1],[1,2], [1,3]], [[2,1],[2,2],[2,3]], [[3,1],[3,2],[3,3]], [[1,1],[2,2],[3,3]], [[1,3], [2,2], [3, 1]], [[[1,1],[2,1], [3,1]], [[1,2],[2,2],[3,2]], [[1,3],[2,3],[3,3]]]];
 function Game() {
   this.turn = 1;
@@ -59,9 +67,22 @@ function Game() {
 // }
 
 
+function test(id) {
+  var idList = id.split(",");
+
+  alert(winArrays[0][1]);
+}
+
+
 $(document).ready(function() {
 
-    $(".test").click(function() {
+    $("button").click(function() {
+      var squareId = $(this).attr("id");
       $(this).prop("disabled", true);
+      test(squareId);
+      console.log(squareId);
     });
 });
+
+
+
